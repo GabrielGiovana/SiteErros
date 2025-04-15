fefd637a145a4136d88ad3c5ab5d134cc604c0a6
GabrielGiovana <projeto8910@gmail.com>
2025-04-15 19:15:53 -0300

    adicionada a tela de cadastro

--- Main.py
+++ Main.py
@@ -32,6 +32,9 @@ def novo_problema():
       return render_template("teste.html")
 
 
+@app.route("/")
+def home():
+    return render_template("index.html") 
 
 if __name__ == "__main__":
     app.run(debug=True)
